8e8120c37766d851d5115892b5cc9c977a11b418
Aigne <aigne@amb-tp.home>
2024-03-30 01:24:20 -0400

    fixed set reg and print binary functions

--- bit_manipulation.c
+++ bit_manipulation.c
@@ -1,4 +1,16 @@
 // Bit Manipulation Source
 
-#include bit_manipulation.h
+#include "bit_manipulation.h"
 
+void printb8(uint8_t b) {
+    int mask = 0b00000001;
+    for(int i = 0; i < 8; i ++) {
+        if(b & 0b10000000) {
+            printf("1");
+        }
+        else {
+            printf("0");
+        }
+        b = b << 1;
+    }
+}
--- bit_manipulation.h
+++ bit_manipulation.h
@@ -1,4 +1,8 @@
 // Bit Manipulation Header
+/* Mandatory Reqiurements
+ * <stdint>
+ * <stdio>
+ */
 
 #ifndef BIT_MANIPULATION_H
 #define BIT_MANIPULATION_H
@@ -7,11 +11,12 @@
 #define sbi(reg, bit) (reg |= (1 << bit))  // set bit of register reg
 #define cbi(reg, bit) (reg &= ~(1 << bit)) // clear bit of register reg
 
-#define sbs(reg, mask, values) (reg &= (~mask & values))
+#define sbs(reg, mask, values) (reg = (reg | values) & (~mask | values))
 /*  Example
 
 
    */
 
+void printb8(uint8_t b);
 
 #endif // BIT_MANIPULATION_H 
